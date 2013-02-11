85c526ea2fca4c20f1f0775f40720cae32145ed7
Andy Huang <andy.huang@terrandin.com>
2013-02-11 12:22:33 -0800

    Basic PlotModel. I'm sure this will grow later, but for now, it addresses the basic needs: Who owns it, where is it, and who can work on it.

---
+++ src/main/java/net/chiisana/builddit/model/PlotModel.java
@@ -0,0 +1,24 @@
+package net.chiisana.builddit.model;
+
+import java.util.HashSet;
+
+public class PlotModel {
+	public String owner;
+
+	public int plotX;
+	public int plotY;
+
+	private HashSet<String> authorized = new HashSet<String>();
+
+	public void addAuthorized(String user) {
+		this.authorized.add(user);
+	}
+
+	public void removeAuthorized(String user) {
+		this.authorized.remove(user);
+	}
+
+	public boolean isAuthorized(String user) {
+		return this.authorized.contains(user);
+	}
+}
