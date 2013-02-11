a34849c35231f4afd4afdece642a2ea94cb1ca78
Andy Huang <andy.huang@terrandin.com>
2013-02-10 20:15:55 -0800

    Debug outputs, this is going to spam the console.

--- src/main/java/net/chiisana/builddit/generator/PlotGenerator.java
+++ src/main/java/net/chiisana/builddit/generator/PlotGenerator.java
@@ -1,5 +1,6 @@
 package net.chiisana.builddit.generator;
 
+import net.chiisana.builddit.Builddit;
 import net.chiisana.builddit.model.PlotConfiguration;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -10,6 +11,7 @@ import org.bukkit.generator.ChunkGenerator;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.logging.Level;
 
 public class PlotGenerator extends ChunkGenerator {
 	@Override
@@ -29,14 +31,19 @@ public class PlotGenerator extends ChunkGenerator {
 
 	@Override
 	public short[][] generateExtBlockSections(World world, Random random, int cx, int cz, BiomeGrid biomes) {
+		Builddit.getInstance().getLogger().log(Level.INFO, "Entering generateExtBlockSections");
+
 		int height = PlotConfiguration.intPlotHeight + 2;
 		int cxx = cx << 4;
 		int czz = cz << 4;
 
 		short[][] result = new short[world.getMaxHeight()/16][];
 		for (int x = 0; x < 16; x++) {
+			Builddit.getInstance().getLogger().log(Level.INFO, "...for x: " + x);
 			for (int z = 0; z < 16; z++) {
+				Builddit.getInstance().getLogger().log(Level.INFO, "...for z: " + z);
 				for (int y = 0; y < height; y++) {
+					Builddit.getInstance().getLogger().log(Level.INFO, "...for y: " + y);
 					if (y == 0) {
 						// Base Layer
 						setBlock(result, x, y, z, (short)PlotConfiguration.materialBase.getId());
--- src/main/java/net/chiisana/builddit/generator/PlotPopulator.java
+++ src/main/java/net/chiisana/builddit/generator/PlotPopulator.java
@@ -1,5 +1,6 @@
 package net.chiisana.builddit.generator;
 
+import net.chiisana.builddit.Builddit;
 import net.chiisana.builddit.model.PlotConfiguration;
 import org.bukkit.Chunk;
 import org.bukkit.World;
@@ -7,17 +8,22 @@ import org.bukkit.block.Biome;
 import org.bukkit.generator.BlockPopulator;
 
 import java.util.Random;
+import java.util.logging.Level;
 
 public class PlotPopulator extends BlockPopulator {
 	@Override
 	public void populate(World world, Random random, Chunk chunk) {
+		Builddit.getInstance().getLogger().log(Level.INFO, "Entering populate");
 		int cxx = chunk.getX() << 4;
 		int czz = chunk.getZ() << 4;
 
 		for (int x = 0; x < 16; x++) {
+			Builddit.getInstance().getLogger().log(Level.INFO, "...for x: " + x);
 			for (int z = 0; z < 16; x++) {
+				Builddit.getInstance().getLogger().log(Level.INFO, "...for z: " + z);
 				world.setBiome(x + cxx, z + czz, Biome.PLAINS);
 				for (int y = 0; y < PlotConfiguration.intPlotHeight + 2; y++) {
+					Builddit.getInstance().getLogger().log(Level.INFO, "...for y: " + y);
 					if (y == 0) {
 						// Base Layer
 						setBlockData(world, x, y, z, PlotConfiguration.byteMaterialBaseData);
