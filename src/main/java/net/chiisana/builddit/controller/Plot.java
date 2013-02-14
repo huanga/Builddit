386f398627d90dcfab9e61aba780e66a74ac6666
Andy Huang <andy.huang@terrandin.com>
2013-02-14 11:48:49 -0800

    Following WE API recommendation to get the edit session correctly. Added debug output to show whether or not top/bottom locations are identified.

--- src/main/java/net/chiisana/builddit/controller/Plot.java
+++ src/main/java/net/chiisana/builddit/controller/Plot.java
@@ -13,6 +13,8 @@ import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
+import java.util.logging.Level;
+
 public class Plot {
 	private PlotModel model;
 
@@ -42,21 +44,25 @@ public class Plot {
 	}
 
 	public Location getBottom() {
-		return(new Location(
+		Location location = new Location(
 					this.getWorld(),
 					this.getPlotX() * PlotConfiguration.intPlotCalculatedSize,
 					0,
 					this.getPlotZ() * PlotConfiguration.intPlotCalculatedSize
-		));
+		);
+		Builddit.getInstance().getLogger().log(Level.INFO, "Bottom identified at: " + location.toString());
+		return location;
 	}
 
 	public Location getTop() {
-		return(new Location(
+		Location location = new Location(
 					this.getWorld(),
 					((this.getPlotX()+1) * PlotConfiguration.intPlotCalculatedSize) - 1,
 					this.getWorld().getMaxHeight(),
 					((this.getPlotZ()+1) * PlotConfiguration.intPlotCalculatedSize) - 1
-		));
+		);
+		Builddit.getInstance().getLogger().log(Level.INFO, "Top identified at: " + location.toString());
+		return location;
 	}
 
 	public boolean claim(String owner) {
@@ -117,25 +123,25 @@ public class Plot {
 	public boolean clear(Player clearer) {
 		if (this.getOwner().equals(clearer.getName()) || clearer.hasPermission("builddit.admin"))
 		{
+			WorldEditPlugin wePlugin = Builddit.getInstance().wePlugin;
+			WorldEditAPI weAPI = Builddit.getInstance().weAPI;
+
+			LocalPlayer player = wePlugin.wrapPlayer(clearer);
+
 			Location location1 = this.getBottom();
 			Location location2 = this.getTop();
 
 			Vector vPos1 = new Vector(location1.getX(), location1.getY(), location1.getZ());
 			Vector vPos2 = new Vector(location2.getX(), location2.getY(), location2.getZ());
 
-			WorldEditPlugin wePlugin = Builddit.getInstance().wePlugin;
-			WorldEditAPI weAPI = Builddit.getInstance().weAPI;
-
-			EditSession editSession = wePlugin.createEditSession(clearer);
 			LocalSession session = weAPI.getSession(clearer);
-
+			EditSession editSession = session.createEditSession(player);
 			RegionSelector regionSelector = session.getRegionSelector(editSession.getWorld());
-			regionSelector.selectPrimary(vPos1);
-			regionSelector.selectSecondary(vPos2);
+			regionSelector.explainPrimarySelection(player, session, vPos1);
+			regionSelector.explainSecondarySelection(player, session, vPos2);
 
 			try {
 				// Note: Lifted code from WorldEdit as there is no API for regenerating a selection
-				LocalPlayer player = wePlugin.wrapPlayer(clearer);
 				Region region = regionSelector.getRegion();
 				Mask mask = session.getMask();
 				session.setMask(null);
