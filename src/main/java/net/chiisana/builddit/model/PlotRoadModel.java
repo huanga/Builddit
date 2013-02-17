d36e25657c6c52d5378960feba55a56fc615468b
Andy Huang <andy.huang@terrandin.com>
2013-02-17 14:52:19 -0800

    Need this for the Plot neighbour-awareness.

---
+++ src/main/java/net/chiisana/builddit/model/PlotRoadModel.java
@@ -0,0 +1,40 @@
+package net.chiisana.builddit.model;
+
+public class PlotRoadModel {
+	private int roadXMin;
+	private int roadXMax;
+	private int roadZMin;
+	private int roadZMax;
+
+	public int getRoadXMin() {
+		return roadXMin;
+	}
+
+	public void setRoadXMin(int roadXMin) {
+		this.roadXMin = roadXMin;
+	}
+
+	public int getRoadXMax() {
+		return roadXMax;
+	}
+
+	public void setRoadXMax(int roadXMax) {
+		this.roadXMax = roadXMax;
+	}
+
+	public int getRoadZMin() {
+		return roadZMin;
+	}
+
+	public void setRoadZMin(int roadZMin) {
+		this.roadZMin = roadZMin;
+	}
+
+	public int getRoadZMax() {
+		return roadZMax;
+	}
+
+	public void setRoadZMax(int roadZMax) {
+		this.roadZMax = roadZMax;
+	}
+}
